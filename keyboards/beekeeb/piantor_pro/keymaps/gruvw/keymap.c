49f4d7d932de0145a8886386f6953a46d32d6d80
gruvw <gruvw.dev@gmail.com>
2025-01-20 11:31:27 +0100

    feat: working oneshot mods + layers

--- keyboards/beekeeb/piantor_pro/keymaps/gruvw/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/gruvw/keymap.c
@@ -1,57 +1,46 @@
 #include QMK_KEYBOARD_H
 
-// === settings ===
+// === special keys ===
 
-#define ONESHOT_TIMEOUT 5000
+#define NU_LAY LT(1, KC_ENT)
+#define SP_LAY LT(2, KC_TAB)
+#define MO_LAY LT(3, MOD_LCTL) // LT(3, OSM(MOD_LCTL))
+#define MA_LAY LT(4, MOD_LSFT) // LT(4, OSM(MOD_LSFT))
+
+#define OS_LALT OSM(MOD_LALT)
+#define OS_LGUI OSM(MOD_LGUI)
 
 // === custom keys ===
 
 enum custom_keycodes {
-    MO_LAY = SAFE_RANGE, // LT(3, OSM(MOD_LSFT))
-    MA_LAY, // LT(4, OSM(MOD_LCTL))
-    MC_WEB, // https://
-    MC_MAIL, // gruvw.dev@gmail.com
-    MC_SIGN, // Lucas Jung (@gruvw)\nhttps://gruvw.com
+    MA_WEB = SAFE_RANGE, // https://
+    MA_MAIL, // gruvw.dev@gmail.com
+    MA_SIGN, // Lucas Jung (@gruvw)\nhttps://gruvw.com
 };
-
-static uint16_t mo_lay_timer;
-static uint16_t ma_lay_timer;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case MO_LAY:
-            if (record->event.pressed) {
-                mo_lay_timer = timer_read();
-                layer_on(3);
-            } else {
-                layer_off(3);
-                if (timer_elapsed(mo_lay_timer) < TAPPING_TERM) {
-                    set_oneshot_mods(MOD_LCTL);
-                }
+            if (record->event.pressed && record->tap.count > 0) {
+                add_oneshot_mods(MOD_LCTL);
             }
-            return false;
+            break;
         case MA_LAY:
-            if (record->event.pressed) {
-                ma_lay_timer = timer_read();
-                layer_on(4);
-            } else {
-                layer_off(4);
-                if (timer_elapsed(ma_lay_timer) < TAPPING_TERM) {
-                    set_oneshot_mods(MOD_LSFT);
-                }
+            if (record->event.pressed && record->tap.count > 0) {
+                add_oneshot_mods(MOD_LSFT);
             }
-            return false;
-        case MC_WEB:
+            break;
+        case MA_WEB:
             if (record->event.pressed) {
                 SEND_STRING("https://");
             }
             break;
-        case MC_MAIL:
+        case MA_MAIL:
             if (record->event.pressed) {
                 SEND_STRING("gruvw.dev@gmail.com");
             }
             break;
-        case MC_SIGN:
+        case MA_SIGN:
             if (record->event.pressed) {
                 SEND_STRING("Lucas Jung (@gruvw)\nhttps://gruvw.com");
             }
@@ -77,14 +66,6 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 }
 
-// === special keys ===
-
-#define SP_LAY LT(1, KC_ENT)
-#define NU_LAY LT(2, KC_TAB)
-
-#define OS_LALT OSM(MOD_LALT)
-#define OS_LGUI OSM(MOD_LGUI)
-
 // === unicode keys ===
 
 #define UC_DEG UC(0x00B0) // °
@@ -92,9 +73,11 @@ bool caps_word_press_user(uint16_t keycode) {
 
 // === keymap - 36 keys ===
 
-// TODO Combine one shot keys
-// TODO Cancel one shot keys
-// TODO make sure repeatable shortcuts are easaly repeatable (hold keys), home row mods ?
+// TODO set bootloader name `gruvw-keyboard`
+// TODO Fast typing lay(osm) with key (within typing term), needs study of common chains
+// - ctrl+tap
+// - ctrl+shift+tab
+// - gui+ctrl+h/j/k/l
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // layer 0: letters, spacing, modifiers
@@ -102,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,   OS_LALT, OS_LGUI, KC_DOT,  KC_P,    KC_Y,                          KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_NO,
         KC_NO,   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                          KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_NO,
         KC_NO,   KC_ESC,  KC_Q,    KC_J,    KC_K,    KC_X,                          KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_NO,
-                                            MO_LAY,  KC_SPC,  NU_LAY,      SP_LAY,  KC_BSPC, MA_LAY
+                                            MO_LAY,  KC_SPC,  SP_LAY,      NU_LAY,  KC_BSPC, MA_LAY
     ),
 
     // layer 1: numbers, special character
@@ -113,12 +96,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             KC_RALT, KC_EQL,  KC_SCLN,     _______, KC_NO,   KC_NO
     ),
 
-    // layer 2: special characters, function keys
+    // layer 2: special characters, function keys, caps word
     [2] = LAYOUT_split_3x6_3(
         KC_NO,   KC_PSCR, XXXXXXX, KC_F10,  KC_F11,  KC_F12,                        UC_NEXT, UC_DEG,  UC_MID,  XXXXXXX, XXXXXXX, KC_NO,
         KC_NO,   XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,                         KC_PIPE, KC_LPRN, KC_LBRC, KC_LCBR, KC_LABK, KC_NO,
         KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,                         XXXXXXX, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, KC_NO,
-                                            KC_NO,   KC_NO,   _______,     KC_NO,   KC_NO,   KC_NO
+                                            KC_NO,   KC_NO,   _______,     KC_NO,   KC_NO,   CW_TOGG
     ),
 
     // layer 3: mouse, cursor control, sound, brightness, media control
@@ -129,11 +112,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, KC_NO,   KC_NO,       MS_BTN3, MS_BTN1, MS_BTN2
     ),
 
-    // layer 4: macros, pre-defined macros, keyboard boot, caps word
+    // layer 4: macros, predefined macros, keyboard boot
     [4] = LAYOUT_split_3x6_3(
         KC_NO,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, DM_REC1, DM_REC2, XXXXXXX, XXXXXXX, KC_NO,
-        KC_NO,   XXXXXXX, MC_SIGN, MC_MAIL, MC_WEB,  XXXXXXX,                       XXXXXXX, DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, KC_NO,
+        KC_NO,   XXXXXXX, MA_SIGN, MA_MAIL, MA_WEB,  XXXXXXX,                       XXXXXXX, DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, KC_NO,
         KC_NO,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, KC_NO,
-                                            KC_NO,   CW_TOGG, KC_NO,       KC_NO,   KC_NO,   _______
+                                            KC_NO,   KC_NO,   KC_NO,       KC_NO,   KC_NO,   _______
     ),
 };
